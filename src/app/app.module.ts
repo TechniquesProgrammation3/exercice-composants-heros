098e6e38a30b1dca303b5216bdca510d88a39599
Carine Croteau <croteau.carine@cegepvicto.ca>
2020-10-21 19:29:07 -0400

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { EnteteComponent } from './entete/entete.component';
+import { RechercheComponent } from './recherche/recherche.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { HeroComponent } from './hero/hero.component';
+import { PiedPageComponent } from './pied-page/pied-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Page1Component,
+    Page2Component,
+    EnteteComponent,
+    RechercheComponent,
+    CarouselComponent,
+    HeroComponent,
+    PiedPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
